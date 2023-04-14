3b6dc248180574df47b9cbc3cc392ebe7e5a681f
Alexander Goscinski <alex.goscinski@posteo.de>
2023-04-14 16:28:38 +0200

    add widget cue box and styles
    * add css styles and function that sets them
    * add tests for widget cue box
    * add .ipynb to .gitignore

---
+++ src/scwidgets/_widget_cue_box.py
@@ -0,0 +1,184 @@
+from typing import List, Optional, Union
+
+from ipywidgets import Box, Widget
+from traitlets.utils.sentinel import Sentinel
+
+
+class CueBox(Box):
+    """
+    A box around the widget :param widget_to_cue: that adds a visual cue defined in the
+    :param css_style: when the trait :param traits_to_observe: in the widget :param
+    widget_to_observe: changes.
+
+    :param widget_to_observe:
+        The widget to observa if the :param traits_to_observe: has changed.
+    :param traits_to_observe:
+        The trait from the :param widget_to_observe: to observe if changed.
+        Specify `traitlets.All` to observe all traits.
+    :param widget_to_cue:
+        The widget to wrap the box around to give a visual cue, once :param
+        traits_to_observe: has changed
+        If None, then the :param widget_to_cue: is set to :param widget_to_observe:.
+    :param css_syle:
+        - **initialization**: the css style of the box during initialization
+        - **on_trait_changed**: the css style that is added when :param
+          traits_to_observe: in widget :param widget_to_observe: changes.
+          It is supposed to change the style of the box such that the user has a visual
+          cue that :param widget_to_cue: has changed.
+
+    Further accepts the same (keyword) arguments as :py:class:`ipywidgets.Box`.
+    """
+
+    def __init__(
+        self,
+        widget_to_observe: Widget,
+        traits_to_observe: Union[str, List[str], Sentinel, None] = "value",
+        widget_to_cue: Optional[Widget] = None,
+        css_style: Optional[dict] = None,
+        *args,
+        **kwargs,
+    ):
+        self._widget_to_observe = widget_to_observe
+        self._traits_to_observe = traits_to_observe
+
+        if widget_to_cue is None:
+            self._widget_to_cue = widget_to_observe
+        else:
+            self._widget_to_cue = widget_to_cue
+
+        self._widget_to_observe = widget_to_observe
+
+        if css_style is None:
+            self._css_style = {
+                "base": "scwidget-cue-box",
+                "cue": "scwidget-cue-box--cue",
+            }
+        else:
+            self._css_style = css_style
+
+        super().__init__([self._widget_to_cue], *args, **kwargs)
+
+        self._widget_to_observe.observe(
+            self._on_traits_to_observe_changed, traits_to_observe
+        )
+        self.add_class(self._css_style["base"])
+
+    @property
+    def widget_to_observe(self):
+        return self._widget_to_observe
+
+    @property
+    def traits_to_observe(self):
+        return self._traits_to_observe
+
+    @property
+    def widget_to_cue(self):
+        return self._widget_to_cue
+
+    def _on_traits_to_observe_changed(self, change: dict):
+        self.add_class(self._css_style["cue"])
+
+    def remove_cue(self):
+        self.remove_class(self._css_style["cue"])
+
+
+class SaveCueBox(CueBox):
+    """
+    A box around the widget :param widget_to_cue: that adds a visual cue defined in the
+    :param css_style: when the trait :param traits_to_observe: in the widget :param
+    widget_to_observe: changes.
+
+    :param widget_to_observe:
+        The widget to observa if the :param traits_to_observe: has changed.
+    :param traits_to_observe:
+        The trait from the :param widget_to_observe: to observe if changed.
+        Specify `traitlets.All` to observe all traits.
+    :param widget_to_cue:
+        The widget to wrap the box around to give a visual cue, once :param
+        traits_to_observe: has changed
+        If None, then the :param widget_to_cue: is set to :param widget_to_observe:.
+
+    Further accepts the same (keyword) arguments as :py:class:`ipywidgets.Box`.
+    """
+
+    def __init__(
+        self,
+        widget_to_observe: Widget,
+        traits_to_observe: Union[str, List[str], Sentinel] = "value",
+        widget_to_cue: Optional[Widget] = None,
+        *args,
+        **kwargs,
+    ):
+        css_style = {
+            "base": "scwidget-save-cue-box",
+            "cue": "scwidget-save-cue-box--cue",
+        }
+        super().__init__(widget_to_observe, traits_to_observe, widget_to_cue, css_style)
+
+
+class CheckCueBox(CueBox):
+    """
+    A box around the widget :param widget_to_cue: that adds a visual cue defined in the
+    :param css_style: when the trait :param traits_to_observe: in the widget :param
+    widget_to_observe: changes.
+
+    :param widget_to_observe:
+        The widget to observa if the :param traits_to_observe: has changed.
+    :param traits_to_observe:
+        The trait from the :param widget_to_observe: to observe if changed.
+        Specify `traitlets.All` to observe all traits.
+    :param widget_to_cue:
+        The widget to wrap the box around to give a visual cue, once :param
+        traits_to_observe: has changed
+        If None, then the :param widget_to_cue: is set to :param widget_to_observe:.
+
+    Further accepts the same (keyword) arguments as :py:class:`ipywidgets.Box`.
+    """
+
+    def __init__(
+        self,
+        widget_to_observe: Widget,
+        traits_to_observe: Union[str, List[str], Sentinel] = "value",
+        widget_to_cue: Optional[Widget] = None,
+        *args,
+        **kwargs,
+    ):
+        css_style = {
+            "base": "scwidget-check-cue-box",
+            "cue": "scwidget-check-cue-box--cue",
+        }
+        super().__init__(widget_to_observe, traits_to_observe, widget_to_cue, css_style)
+
+
+class UpdateCueBox(CueBox):
+    """
+    A box around the widget :param widget_to_cue: that adds a visual cue defined in the
+    :param css_style: when the trait :param traits_to_observe: in the widget :param
+    widget_to_observe: changes.
+
+    :param widget_to_observe:
+        The widget to observa if the :param traits_to_observe: has changed.
+    :param traits_to_observe:
+        The trait from the :param widget_to_observe: to observe if changed.
+        Specify `traitlets.All` to observe all traits.
+    :param widget_to_cue:
+        The widget to wrap the box around to give a visual cue, once :param
+        traits_to_observe: has changed
+        If None, then the :param widget_to_cue: is set to :param widget_to_observe:.
+
+    Further accepts the same (keyword) arguments as :py:class:`ipywidgets.Box`.
+    """
+
+    def __init__(
+        self,
+        widget_to_observe: Widget,
+        traits_to_observe: Union[str, List[str], Sentinel] = "value",
+        widget_to_cue: Optional[Widget] = None,
+        *args,
+        **kwargs,
+    ):
+        css_style = {
+            "base": "scwidget-update-cue-box",
+            "cue": "scwidget-update-cue-box--cue",
+        }
+        super().__init__(widget_to_observe, traits_to_observe, widget_to_cue, css_style)
--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,4 +1,6 @@
+import pytest
 import requests
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
@@ -14,12 +16,54 @@ def test_notebook_running(notebook_service):
     assert response.status_code == 200
 
 
-def test_widgets(selenium_driver):
+def test_widget_cue_box(selenium_driver):
     """
     Basic test checks if button with description "Text" exists
 
     :param selenium_driver: see conftest.py
     """
-    driver = selenium_driver("tests/notebooks/widgets.ipynb")
+    driver = selenium_driver("tests/notebooks/widget_cue_box.ipynb")
 
-    driver.find_element(By.XPATH, '//Button[text()="Text"]')
+    # Each cell of the notebook, the cell number can be retrieved from the
+    # attribute "data-windowed-list-index"
+    nb_cells = driver.find_elements(
+        By.CLASS_NAME, "lm-Widget.jp-Cell.jp-CodeCell.jp-Notebook-cell"
+    )
+
+    # Checks if the labels widget with value "Text" exists in cell 2
+    widget1 = nb_cells[2].find_element(
+        By.CLASS_NAME, "lm-Widget.jupyter-widgets.widget-inline-hbox.widget-label"
+    )
+    assert widget1.text == "Text"
+
+    # Checks if the labels widget with unchanged value has the correct css style
+    nb_cells[2].find_element(
+        By.CLASS_NAME,
+        "lm-Widget.lm-Panel.jupyter-widgets.widget-container"
+        ".widget-box.scwidget-cue-box.scwidget-cue-box",
+    )
+    # we assume error is raised because the widget should not be cued
+    with pytest.raises(NoSuchElementException, match=r".*Unable to locate element.*"):
+        nb_cells[2].find_element(
+            By.CLASS_NAME,
+            "lm-Widget.lm-Panel.jupyter-widgets.widget-container"
+            ".widget-box.scwidget-cue-box.scwidget-cue-box--cue",
+        )
+
+    # Checks if the labels widget with value "Cahnged Text" exists in cell 3
+    widget2 = nb_cells[3].find_element(
+        By.CLASS_NAME, "lm-Widget.jupyter-widgets.widget-inline-hbox.widget-label"
+    )
+    assert widget2.text == "Changed Text"
+
+    # Checks if the labels widget with changed value has the correct css style
+    nb_cells[3].find_element(
+        By.CLASS_NAME,
+        "lm-Widget.lm-Panel.jupyter-widgets.widget-container.widget-box"
+        ".scwidget-cue-box.scwidget-cue-box",
+    )
+    nb_cells[3].find_element(
+        By.CLASS_NAME,
+        "lm-Widget.lm-Panel.jupyter-widgets.widget-container.widget-box"
+        ".scwidget-cue-box.scwidget-cue-box--cue",
+    )
