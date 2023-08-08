e54930a9386574ff2e043a40a6e2f24afcd9d61b
Alexander Goscinski <alex.goscinski@posteo.de>
2023-08-08 20:49:41 +0200

    fix linter errors in docs/src/conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,22 +1,24 @@
 # Sphinx documentation build configuration file
 
-import os
-import re
-import time
-
 import scwidgets
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-              'sphinx.ext.autosummary', 'sphinx.ext.extlinks',
-              'sphinx.ext.intersphinx',
-              'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.inheritance_diagram",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build']
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
 
-project = 'scwidget'
-copyright = f'BSD 3-Clause License, Copyright (c) 2023, scicode-widgets developer team'
+project = "scwidget"
+copyright = "BSD 3-Clause License, Copyright (c) 2023, scicode-widgets developer team"
 version = scwidgets.__version__
 release = version
 
-htmlhelp_basename = 'scicode-widget-doc'
+htmlhelp_basename = "scicode-widget-doc"
