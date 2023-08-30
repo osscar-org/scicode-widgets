cc03ebf8b693b71c3ab0ad134f200d9d2f191d9f
Alexander Goscinski <alex.goscinski@posteo.de>
2023-08-30 19:26:06 +0200

    rm nonfunction code documentation
    * rm forgotton TODO in doc
    
    * rm jupytext doc in wrong file

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.0
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 from typing import List
 
 import pytest
