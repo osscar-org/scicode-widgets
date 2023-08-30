d5d6e4ca8f571c170705d9ff6d04463b4c3da0ef
Alexander Goscinski <alex.goscinski@posteo.de>
2023-08-31 00:03:01 +0200

    restructure code demo test utils for more coverage
    To conside more cases for the upcoming tests, hypeparameters to set up a
    code demo are introducet into the tests. No actual new tests have been
    added in this commit. Readability has been improved.

--- tests/notebooks/widget_code_demo.py
+++ tests/notebooks/widget_code_demo.py
@@ -28,8 +28,8 @@ from tests.test_code import get_code_demo  # noqa: E402
 scwidgets.get_css_style()
 
 
-def run_code_demo(checks):
-    return get_code_demo(checks)
+def run_code_demo(checks, include_checks, include_params, tunable_params):
+    return get_code_demo(checks, include_checks, include_params, tunable_params)
 
 
 # Test 1:
@@ -37,10 +37,62 @@ def run_code_demo(checks):
 # Test if CodeDemo shows correct output
 
 # Test 1.1
-run_code_demo([single_param_check(use_fingerprint=False, failing=False, buggy=False)])
+run_code_demo(
+    [single_param_check(use_fingerprint=False, failing=False, buggy=False)],
+    include_checks=True,
+    include_params=True,
+    tunable_params=False,
+)
 
 # Test 1.2
-run_code_demo([single_param_check(use_fingerprint=False, failing=True, buggy=False)])
+run_code_demo(
+    [single_param_check(use_fingerprint=False, failing=True, buggy=False)],
+    include_checks=True,
+    include_params=True,
+    tunable_params=False,
+)
 
 # Test 1.3
-run_code_demo([single_param_check(use_fingerprint=False, failing=False, buggy=True)])
+run_code_demo(
+    [single_param_check(use_fingerprint=False, failing=False, buggy=True)],
+    include_checks=True,
+    include_params=True,
+    tunable_params=False,
+)
+
+# Test 1.4
+# run_code_demo(
+#    [single_param_check(use_fingerprint=False, failing=False, buggy=False)],
+#    include_checks=True,
+#    include_params=True,
+#    tunable_params=True,
+# )
+
+
+# Test 2:
+# -------
+# Test if CodeDemo works correct for only checks
+
+# Test 2.1
+# TODO
+# run_code_demo([single_param_check(use_fingerprint=False, failing=True, buggy=False)],
+#        include_checks=True, include_params=False)
+
+# Test 2.2
+# TODO
+# run_code_demo([single_param_check(use_fingerprint=True, failing=True, buggy=False)],
+#        include_checks=True, include_params=False)
+
+# Test 3:
+# -------
+# Test if CodeDemo works correct for only update
+
+# Test 3.1
+# TODO
+# run_code_demo([single_param_check(use_fingerprint=False, failing=True, buggy=False)],
+#        include_checks=False, include_params=True, tunable_params)
+
+# Test 3.2
+# TODO
+# run_code_demo([single_param_check(use_fingerprint=True, failing=True, buggy=False)],
+#        include_checks=False, include_params=True, tunable_params)
