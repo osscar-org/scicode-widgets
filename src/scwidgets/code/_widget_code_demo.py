285bc5a0d02afa72d86a4b7f253ac32455bef967
Alexander Goscinski <alex.goscinski@posteo.de>
2023-09-04 16:26:09 +0200

    replace generic ResetCueButtons with respective type in code demo
    The cueing of the ResetCueButton replaces the the cueing by the CueBox

--- src/scwidgets/code/_widget_code_demo.py
+++ src/scwidgets/code/_widget_code_demo.py
@@ -6,7 +6,7 @@ from ipywidgets import HBox, Layout, Output, VBox, Widget
 from widget_code_input.utils import CodeValidationError
 
 from ..check import Check, CheckableWidget, CheckRegistry, ChecksLog
-from ..cue import CheckCueBox, ResetCueButton, UpdateCueBox
+from ..cue import CheckCueBox, CheckResetCueButton, UpdateCueBox, UpdateResetCueButton
 from ._widget_code_input import CodeInput
 from ._widget_parameter_panel import ParameterPanel
 
@@ -49,30 +49,20 @@ class CodeDemo(VBox, CheckableWidget):
 
         if self._check_registry is None:
             self._check_button = None
-            self._cue_check_button = None
         else:
-            self._check_button = ResetCueButton(
-                [],
+            self._cue_code = CheckCueBox(
+                self._code, "function_body", self._cue_code, cued=True
+            )
+            self._check_button = CheckResetCueButton(
+                [self._cue_code],
                 self._on_click_check_action,
                 description="Check Code",
                 disable_on_successful_action=False,
                 button_tooltip="Check the correctness of your code",
             )
-            self._cue_code = CheckCueBox(
-                self._code, "function_body", self._cue_code, cued=True
-            )
-            self._cue_check_button = CheckCueBox(
-                self._code,
-                "function_body",
-                self._check_button,
-                cued=True,
-                layout=Layout(width="98%", height="auto"),
-            )
-            self._check_button.cue_widgets = [self._cue_code, self._cue_check_button]
 
         if self._parameters is None:
             self._update_button = None
-            self._cue_update_button = None
             self._parameter_panel = VBox([])
         else:
             compatibility_result = self._code.compatible_with_signature(
@@ -83,13 +73,6 @@ class CodeDemo(VBox, CheckableWidget):
 
             # set up update button and cueing
             # -------------------------------
-            self._update_button = ResetCueButton(
-                [],
-                self._on_click_update_action,
-                disable_on_successful_action=False,
-                description="Run Code",
-                button_tooltip="Runs the code with the specified parameters",
-            )
             self._cue_code = UpdateCueBox(
                 self._code,
                 "function_body",
@@ -106,30 +89,23 @@ class CodeDemo(VBox, CheckableWidget):
                 ["value"] * len(self._parameter_panel.parameters_widget),
                 self._parameter_panel,
             )
-            self._cue_update_button = UpdateCueBox(
-                [self._code] + self._parameter_panel.parameters_widget,
-                ["function_body"]
-                + ["value"] * len(self._parameter_panel.parameters_widget),
-                self._update_button,
-                cued=True,
-            )
 
-            self._update_button.cue_widgets = [
-                self._cue_code,
-                self._cue_update_button,
-                self._cue_parameter_panel,
-            ]
+            self._update_button = UpdateResetCueButton(
+                [self._cue_code, self._cue_parameter_panel],
+                self._on_click_update_action,
+                disable_on_successful_action=False,
+                description="Run Code",
+                button_tooltip="Runs the code with the specified parameters",
+            )
 
         if self._check_button is None and self._update_button is None:
             self._buttons_panel = HBox([])
         elif self._check_button is None:
-            self._buttons_panel = HBox([self._cue_update_button])
+            self._buttons_panel = HBox([self._update_button])
         elif self._update_button is None:
-            self._buttons_panel = HBox([self._cue_check_button])
+            self._buttons_panel = HBox([self._check_button])
         else:
-            self._buttons_panel = HBox(
-                [self._cue_check_button, self._cue_update_button]
-            )
+            self._buttons_panel = HBox([self._check_button, self._update_button])
 
         VBox.__init__(
             self,
