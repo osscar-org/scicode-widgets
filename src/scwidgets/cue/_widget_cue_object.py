bb2aeb00eccd0e7637650140f1d9e134901b3905
Alexander Goscinski <alex.goscinski@posteo.de>
2023-12-14 21:03:04 +0100

    add cued containers for pyobjects matplotlib figures
    * add CueOutput -> (CueObject, CueFigure) with css
    * add regression tests with reference screenshots for the construction of a figure

---
+++ src/scwidgets/cue/_widget_cue_object.py
@@ -0,0 +1,73 @@
+# postpones evaluation of annotations
+# see https://stackoverflow.com/a/33533514
+from __future__ import annotations
+
+from typing import Any, List, Optional, Union
+
+from IPython.display import display
+from ipywidgets import Widget
+from traitlets.utils.sentinel import Sentinel
+
+from ._widget_cue_output import CueOutput
+
+
+class CueObject(CueOutput):
+    """
+    A cued displayable ipywidget.Output for any Python object.  Provides utilities to
+    clear and redraw the object, for example after an update.
+
+    :param display_object:
+        The object to display
+    :param widgets_to_observe:
+        The widget to observe if the :param traits_to_observe: has changed.
+    :param traits_to_observe:
+        The trait from the :param widgets_to_observe: to observe if changed.
+        Specify `traitlets.All` to observe all traits.
+    :param cued:
+        Specifies if it is cued on initialization
+    :param css_syle:
+        - **base**: the css style of the box during initialization
+        - **cue**: the css style that is added when :param
+          traits_to_observe: in widget :param widgets_to_observe: changes.
+          It is supposed to change the style of the box such that the user has a visual
+          cue that :param widget_to_cue: has changed.
+    """
+
+    def __init__(
+        self,
+        display_object: Any,
+        widgets_to_observe: Union[None, List[Widget], Widget] = None,
+        traits_to_observe: Union[
+            None, str, List[str], List[List[str]], Sentinel
+        ] = None,
+        cued: bool = True,
+        css_style: Optional[dict] = None,
+        *args,
+        **kwargs,
+    ):
+        CueOutput.__init__(
+            self,
+            widgets_to_observe,
+            traits_to_observe,
+            cued,
+            css_style,
+            **kwargs,
+        )
+
+        self._display_object = display_object
+        self.draw_display()
+
+    @property
+    def display_object(self):
+        return self._display_object
+
+    @display_object.setter
+    def display_object(self, display_object: Any):
+        self._display_object = display_object
+
+    def clear_display(self, wait=False):
+        self.clear_output(wait=wait)
+
+    def draw_display(self):
+        with self:
+            display(self._display_object)
---
+++ tests/notebooks/widget_cue_figure-ipympl.py
@@ -0,0 +1,132 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from scwidgets.cue import CueFigure
+
+
+def run_cue_figure():
+    fig = plt.figure()
+    fig.canvas.header_visible = False
+    fig.canvas.footer_visible = False
+    fig.canvas.toolbar_visible = False
+    fig.canvas.resizable = False
+    return CueFigure(fig)
+
+
+matplotlib.use("module://ipympl.backend_nbagg")
+
+
+# #### Test 1.1
+# Tests initalization of figure
+
+cf = run_cue_figure()
+cf
+
+# just checks if no error is raised when no axes is present
+cf.clear_figure()
+
+# Tests if plots are properly closed inside the CueFigure
+# so the output in this celll should be empty
+plt.show()
+
+# #### Test 1.2
+# Tests if axis is drawn on figure
+
+cf = run_cue_figure()
+cf
+
+cf.figure.gca()
+
+# axes should be retained using clear_figure
+cf.clear_figure()
+cf.draw_display()
+
+# Tests if plots are properly closed inside the CueFigure
+# so the output in this celll should be empty
+plt.show()
+
+# +
+# cf.figure.savefig("../screenshots/widget_cue_figure/empty_axis.png")
+# -
+
+# #### Test 1.3
+# Tests draw_display
+
+cf = run_cue_figure()
+cf
+
+ax = cf.figure.gca()
+ax.plot([0, 0.5], [0, 0.5])
+cf.draw_display()
+
+# Tests if plots are properly closed
+plt.show()
+
+
+# +
+# screenshot for references
+# cf.figure.savefig("../screenshots/widget_cue_figure/update_figure_plot.png")
+# -
+
+# #### Test 1.4
+# Tests update_figure using set operation
+
+
+# +
+def update_figure(cf, x, y):
+    ax = cf.figure.gca()
+    ax.plot([0, 1], [0, 1])
+    line = ax.lines[0]
+    line.set_data(x, y)
+
+
+cf = run_cue_figure()
+cf
+# -
+
+cf.clear_display()
+update_figure(cf, [0, 0.5], [0, 0.5])
+cf.draw_display()
+
+# Tests if plots are properly closed
+plt.show()
+
+
+# +
+# screenshot for references
+# cf.figure.savefig("../screenshots/widget_cue_figure/update_figure_set.png")
+# -
+
+# #### Test 1.5
+# Tests update_figure using plot operation
+
+
+# +
+def update_figure(cf, x, y):
+    cf.figure.gca().plot(x, y)
+
+
+cf = run_cue_figure()
+cf
+# -
+
+cf.clear_display()
+update_figure(cf, [0, 0.5], [0, 0.5])
+cf.draw_display()
+
+# Tests if plots are properly closed
+plt.show()
