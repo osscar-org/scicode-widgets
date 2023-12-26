7201460eb12da185ffd9b0e358e87fd5c97a2ae9
Alexander Goscinski <alex.goscinski@posteo.de>
2023-12-26 13:11:42 +0100

    add raise ValueError when code is wrong parameter type

--- src/scwidgets/exercise/_widget_code_exercise.py
+++ src/scwidgets/exercise/_widget_code_exercise.py
@@ -7,6 +7,7 @@ from platform import python_version
 from typing import Any, Callable, Dict, List, Optional, Union
 
 from ipywidgets import HTML, Box, HBox, HTMLMath, Layout, VBox, Widget
+from widget_code_input import WidgetCodeInput
 from widget_code_input.utils import CodeValidationError
 
 from .._utils import Formatter
@@ -56,7 +57,7 @@ class CodeExercise(VBox, CheckableWidget, AnswerWidget):
 
     def __init__(
         self,
-        code: Union[None, CodeInput, types.FunctionType] = None,
+        code: Union[None, WidgetCodeInput, types.FunctionType] = None,
         check_registry: Optional[CheckRegistry] = None,
         answer_registry: Optional[AnswerRegistry] = None,
         answer_key: Optional[str] = None,
@@ -120,6 +121,11 @@ class CodeExercise(VBox, CheckableWidget, AnswerWidget):
         # verify if input argument `parameter` is valid
         if isinstance(code, types.FunctionType):
             code = CodeInput(function=code)
+        elif code is not None and not (isinstance(code, WidgetCodeInput)):
+            raise TypeError(
+                "For input code expected type None, FunctionType or "
+                f"WidgetCodeInput but got {type(code)!r}"
+            )
 
         # check compability between code and parameters, can only be checked if
         # update_func is not used because we cannot know how the code input is used
