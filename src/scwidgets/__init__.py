0b4dd2f935077c24f3e3e10026174e16a4856ef5
Alexander Goscinski <alex.goscinski@posteo.de>
2024-11-29 15:14:09 +0100

    Support imports of submodules from top level (#75)
    One can now import from top level
    ```
    from scwidget import CodeExercise
    # same as
    from scwidget.exercise import CodeExercise
    ```

--- src/scwidgets/__init__.py
+++ src/scwidgets/__init__.py
@@ -2,5 +2,46 @@ __version__ = "0.0.0-dev"
 __authors__ = "the scicode-widgets developer team"
 
 from ._css_style import CssStyle, get_css_style
+from .check import *  # noqa: F403
+from .code import *  # noqa: F403
+from .cue import *  # noqa: F403
+from .exercise import *  # noqa: F403
 
-__all__ = ["CssStyle", "get_css_style"]
+__all__ = [  # noqa: F405
+    # css_style
+    "CssStyle",
+    "get_css_style",
+    # cue
+    "CueWidget",
+    "CheckCueBox",
+    "CueBox",
+    "SaveCueBox",
+    "UpdateCueBox",
+    "ResetCueButton",
+    "SaveResetCueButton",
+    "CheckResetCueButton",
+    "UpdateResetCueButton",
+    "CueOutput",
+    "CueObject",
+    "CueFigure",
+    # code
+    "CodeInput",
+    "ParameterPanel",
+    # check
+    "Check",
+    "CheckResult",
+    "AssertResult",
+    "CheckRegistry",
+    "CheckableWidget",
+    "assert_equal",
+    "assert_shape",
+    "assert_numpy_allclose",
+    "assert_type",
+    "assert_numpy_floating_sub_dtype",
+    "assert_numpy_sub_dtype",
+    # exercise
+    "CodeExercise",
+    "TextExercise",
+    "ExerciseWidget",
+    "ExerciseRegistry",
+]
